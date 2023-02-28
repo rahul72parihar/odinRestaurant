92523ce35ed3c8c6a7fc34500092dc4df2b43b48
Rahul Parihar <rahul.parihar@jalantechnologies.com>
2023-02-28 21:57:03 +0530

    added homepage and menu with styling

--- src/buttonSection.js
+++ src/buttonSection.js
@@ -1,16 +1,20 @@
-export default function buttonSection() {
+export default function buttonSection(section) {
   const btnContainer = document.createElement("div");
   btnContainer.classList.add("button-container");
 
   const homeBtn = document.createElement("button");
   homeBtn.classList.add("home-button");
   homeBtn.textContent = "HOME";
-  btnContainer.appendChild(homeBtn);
 
   const menuBtn = document.createElement("button");
   menuBtn.textContent = "MENU";
   menuBtn.classList.add("menu-button");
 
+  section === "homepage"
+    ? homeBtn.classList.add("selected-btn")
+    : menuBtn.classList.add("selected-btn");
+
+  btnContainer.appendChild(homeBtn);
   btnContainer.appendChild(menuBtn);
 
   return btnContainer;
